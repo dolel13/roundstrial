149e96e5f7631ace48aea4d8727a15f4439c00a0
Rob Pearlman <robpearlman@gmail.com>
2014-08-27 01:22:49 +1000

    ?

--- run.py
+++ run.py
@@ -3,7 +3,7 @@
 import os
 from rounds import app
 
-#port = int(os.environ.get('PORT', 5000)) 
-#app.run(host='0.0.0.0', port=port)
+port = int(os.environ.get('PORT', 5000)) 
+app.run(host='0.0.0.0', port=port)
 
-app.run(debug=True)
+#app.run(debug=True)
